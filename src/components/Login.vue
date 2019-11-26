7015e1b92360f80ca8259ffaacdbadc674940b79
Horváth Tamás <horvatht@itware.hu>
2019-11-26 13:06:38 +0100

    user to localstorage fixed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -88,8 +88,14 @@ export default {
         'Content-Type': 'application/json',
         'authorization': 'Bearer ' + token
       }
-      const user = await axios.post(process.env.VUE_APP_BASE_URL + ApiRoutes.GET_USER.path, {}, { headers: headers })
-      localStorage.setItem(localStorageKeys.NFL_TIPPER_USER, JSON.stringify(user.data))
+      const userResponse = await axios.post(process.env.VUE_APP_BASE_URL + ApiRoutes.GET_USER.path, {}, { headers: headers })
+      const userToSave = {
+        username: userResponse.data.username,
+        userId: userResponse.data._id,
+        email: userResponse.data.email,
+        leagues: userResponse.data.leagues
+      }
+      localStorage.setItem(localStorageKeys.NFL_TIPPER_USER, JSON.stringify(userToSave))
     },
     goRegister () {
       this.$router.push(Routes.REGISTER.path)
